fe5cd423ac6c6c0f84c16a252f3b3b96850fa3b1
Annihilous <benjaminjnorton@gmail.com>
2019-05-01 21:54:21 -0400

    Rename project to "travels"

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsAppTemplate
+module Travels
   class Application < Rails::Application
     config.generators do | g |
       g.assets          false
